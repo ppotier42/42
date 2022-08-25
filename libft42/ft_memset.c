17c8d96001c3cbd32f6b1ffbfec35cc08b10e5a9
Pedroamiguel <pierre.podnieks@gmail.com>
2022-08-25 17:22:49 +0200

    memset

---
+++ libft42/ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppotier <pierre.podnieks@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 16:25:14 by ppotier           #+#    #+#             */
+/*   Updated: 2022/08/25 17:22:08 by ppotier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	char	*str;
+
+	str = b;
+	while (len > 0)
+	{
+		*str = c;
+		str++;
+		len--;
+	}
+	return (b);
+}
+/*
+int main () {
+	char str[] = "remplacement";
+	printf("debut : %s\n", str);
+	ft_memset(str, '~', 13);
+	printf("fin : %s\n", str);
+   return(0);
+}*/
