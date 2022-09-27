12afbf99525fdb58adbe0b993ebbb0c0dcbbe6c4
Pedroamiguel <pierre.podnieks@gmail.com>
2022-09-27 15:16:22 +0200

    gitignore libft

--- libft42/ft_memmove.c
+++ libft42/ft_memmove.c
@@ -6,21 +6,33 @@
 /*   By: ppotier <pierre.podnieks@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 23:06:42 by ppotier           #+#    #+#             */
-/*   Updated: 2022/09/15 23:11:51 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/09/27 15:12:05 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	
+	char	*dest;
+	char	*source;
+
+	dest = (char *) dst;
+	source = (char *) src;
+	if (source > dest)
+	{
+		while (len > 0)
+			dest[len] = source [len];
+	}
+	else
+		ft_memcpy(dest, source, len);
+	return (dst);
 }
-*/
+
 int main()
 {
-	char src[] = "aled";
+	char src[] = "z";
 	char dst[] = "ca marche comment";
-	memmove(dst, src, 17);
+	ft_memmove(dst, src, 1);
 	printf("result : %s", dst);
 }
--- libft42/libft.h
+++ libft42/libft.h
@@ -6,7 +6,7 @@
 /*   By: ppotier <pierre.podnieks@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 23:44:55 by ppotier           #+#    #+#             */
-/*   Updated: 2022/09/12 23:05:57 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/09/27 14:56:46 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n);
 char	*ft_strrchr(const char *s, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
