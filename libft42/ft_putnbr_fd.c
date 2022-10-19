08a6e2a3a99eb79c160c4e5ed934941152e61e45
Pierre 42 <pierre.podnieks@gmail.com>
2022-10-19 15:32:28 +0200

    putnbr de c mort

--- libft42/ft_putnbr_fd.c
+++ libft42/ft_putnbr_fd.c
@@ -6,28 +6,35 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:00:17 by ppotier           #+#    #+#             */
-/*   Updated: 2022/10/19 14:45:37 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/10/19 15:24:50 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
+		write(fd, "-2147483648", 12);
 	else
 	{
 		if (n < 0)
 		{
-			ft_putchar_fd('-', fd);
-			n = n * -1;
+			putchar_fd('-', fd);
+			n *= -n;
 		}
-		if (n >= 9)
+		if (n >= 10)
 		{
 			ft_putnbr_fd(n / 10, fd);
+			n = n % 10;
 		}
-		ft_putchar_fd((n % 10) + '0', fd);
+		if (n >= 0 && n < 10)
+			putchar_fd((n + '0'), fd);
 	}
 }
 
