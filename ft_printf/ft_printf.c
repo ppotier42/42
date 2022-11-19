bdc221229554258c6833fca41c4cb081075cb36f
Pedroamiguel <pierre.podnieks@gmail.com>
2022-11-19 18:10:18 +0100

    MERCIROBINPOURLED

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,56 +6,56 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:25:26 by ppotier           #+#    #+#             */
-/*   Updated: 2022/11/14 16:02:57 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/11/19 18:01:18 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *input, ...)
+int	ft_printf(const char *format, ...)
 {
 	size_t	i;
+	size_t	dest;
 	va_list	ap;
 
 	i = 0;
-	va_start (ap, input);
-	while (input[i])
+	dest = 0;
+	va_start (ap, format);
+	while (format[i])
 	{
-		if (input[i] == '%' && input[i++] == '%')
-			write(1, "%", 1);
-		else if (input[i] == '%' && input[i++] == 'c')
+		if (format[i] == '%')
 		{
-			ft_putchar_fd(input[i], 1);
+			dest = dest + ft_format(format, i, ap);
+			i++;
 		}
-		else if (input[i] == '%' && input[i++] == 's')
-		{
-			ft_putstr_fd((char *)input, 1);
-		}
-		else if (input[i] == '%' && input[i++] == 'p')
-		{
-			printf("aled");
-		}
-		else if (input[i] == '%' && input[i++] == 'd')
-		{
-			ft_atoi(input);
-		}
-		else if (input[i] == '%' && input[i++] == 'i')
-		{
-			printf("aled");
-		}
-		else if (input[i] == '%' && input[i++] == 'u')
-		{
-			printf("aled");
-		}
-		else if (input[i] == '%' && input[i++] == 'x')
-		{
-			printf("aled");
-		}
-		else if (input[i] == '%' && input[i++] == 'X')
-		{
-			printf("aled");
-		}
+		else
+			dest = dest + ft_putchar_l(format[i]);
 	}
+	i++;
 	va_end (ap);
-	return (1);
+	return (dest);
+}
+
+size_t	ft_format(const char *str, size_t start, va_list ap)
+{
+	size_t	res;
+
+	res = 0;
+	if (str[start] == '%')
+		res = ft_putchar_l(str[start]);
+	else if (str[start] == 'c')
+		res = ft_add_char(ap);
+	else if (str[start] == 's')
+		res = ft_add_str(ap);
+	else if (str[start] == 'p')
+		printf("aled");
+	else if (str[start] == 'd' || str[start] == 'i')
+		res = ft_putnbr_num(ap);
+	else if (str[start] == 'u')
+		printf("aled");
+	else if (str[start] == 'x')
+		printf("aled");
+	else if (str[start] == 'X')
+		printf("aled");
+	return (res);
 }
