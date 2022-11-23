592b5eede93c95803ffd46412a0b33fa3a3b26c7
Pedroamiguel <pierre.podnieks@gmail.com>
2022-11-23 10:56:13 +0100

    biensur

---
+++ GNL/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 10:44:00 by ppotier           #+#    #+#             */
+/*   Updated: 2022/11/23 10:47:25 by ppotier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
