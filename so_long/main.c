86fe83ab2944173ecd68e87f8ba65e3a5fc98ccf
Pedroamiguel <pierre.podnieks@gmail.com>
2022-12-16 14:50:12 +0100

    gitigno

--- so_long/main.c
+++ so_long/main.c
@@ -6,15 +6,27 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:28:22 by ppotier           #+#    #+#             */
-/*   Updated: 2022/12/15 15:36:10 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/12/15 17:48:49 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "mlxOS/mlx.h"
 
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line;
+	int		endian;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World !");
+	img = mlx_new_image(mlx, 1920, 1080);
+	addr = mlx_get_data_addr(img, &bpp, &line, &endian);
+	(void)addr;
+	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+	mlx_loop(mlx);
 }
