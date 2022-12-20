a6513984ce38444f9525df84525142e5a92f702e
Pierre 42 <pierre.podnieks@gmail.com>
2022-12-20 16:06:41 +0100

    try

--- so_long/events.c
+++ so_long/events.c
@@ -6,7 +6,7 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 21:53:09 by ppotier           #+#    #+#             */
-/*   Updated: 2022/12/20 13:29:10 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/12/20 15:57:45 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ int	main(void)
 	char	*relative_path;
 	int		img_width;
 	int		img_height;
+	void	*mlx_win;
 
-	relative_path = "./teapot.xpm";
-	img_width = 600;
-	img_height = 600;
+	relative_path = "./item.xpm";
 	mlx = mlx_init();
 	img = mlx_new_image(mlx, 1920, 1080);
-	img = mlx_xpm_to_image(mlx, &relative_path, &img_width, &img_height);
+	(void)mlx_win;
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, 600, 600, 0);
 	mlx_loop(mlx);
 }
