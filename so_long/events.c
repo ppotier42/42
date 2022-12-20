02e67ab148e596819907656d418a1eff74043aa3
Pierre 42 <pierre.podnieks@gmail.com>
2022-12-20 14:29:49 +0100

    changes

--- so_long/events.c
+++ so_long/events.c
@@ -6,30 +6,25 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 21:53:09 by ppotier           #+#    #+#             */
-/*   Updated: 2022/12/16 22:06:04 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/12/20 13:29:10 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlxOS/mlx.h"
 
-typedef struct s_vars {
-	void	*mlx;
-	void	*win;
-}	t_vars;
-
-int	close(int keycode, t_vars *vars)
-{
-	(void)keycode;
-	mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}
-
 int	main(void)
 {
-	t_vars	vars;
+	void	*mlx;
+	void	*img;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 600, 600, "test");
-	mlx_hook(vars.win, 2, 5, close, &vars);
-	mlx_loop(vars.mlx);
+	relative_path = "./teapot.xpm";
+	img_width = 600;
+	img_height = 600;
+	mlx = mlx_init();
+	img = mlx_new_image(mlx, 1920, 1080);
+	img = mlx_xpm_to_image(mlx, &relative_path, &img_width, &img_height);
+	mlx_loop(mlx);
 }
