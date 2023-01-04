afb61840b37a8ec16f2a80867f071480055fccd6
Pedroamiguel <pierre.podnieks@gmail.com>
2023-01-04 23:43:58 +0100

    push ecole

--- so_long/main.c
+++ so_long/main.c
@@ -6,11 +6,12 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:28:22 by ppotier           #+#    #+#             */
-/*   Updated: 2022/12/20 15:17:27 by ppotier          ###   ########.fr       */
+/*   Updated: 2022/12/26 18:18:02 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlxOS/mlx.h"
+#include <math.h>
 
 typedef struct s_data {
 	void	*img;
@@ -28,18 +29,41 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+void	line(void *mlx, void *win, int begx, int begy, int endx, int endy, int clr)
+{
+	double	deltax;
+	double	deltay;
+	int		pixels;
+	double	pixelx;
+	double	pixely;
+
+	deltax = endx - begx;
+	deltay = endy - begy;
+	pixels = sqrt((deltax * deltax) + (deltay * deltay));
+	deltax /= pixels;
+	deltay /= pixels;
+	pixelx = begx;
+	pixely = begy;
+	while (pixels)
+	{
+		mlx_pixel_put(mlx, win, pixelx, pixely, clr);
+		pixelx += deltax;
+		pixely += deltay;
+		--pixels;
+	}
+}
+
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 400, 400, "so_long");
-	img.img = mlx_new_image(mlx, 400, 400);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	line(mlx, mlx_win, 400, 400, 0, 0, 0x0000FF);
+	line(mlx, mlx_win, 400, 0, 0, 400, 0x0000FF);
+	line(mlx, mlx_win, 0, 200, 400, 200, 0x0000FF);
 	mlx_loop(mlx);
 	return (0);
 }
