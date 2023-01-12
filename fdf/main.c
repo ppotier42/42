6afd79429484d32d17b6f3114f92de288381e6a7
Pierre 42 <pierre.podnieks@gmail.com>
2023-01-12 13:47:46 +0100

    giti

--- fdf/main.c
+++ fdf/main.c
@@ -6,15 +6,16 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:28:22 by ppotier           #+#    #+#             */
-/*   Updated: 2023/01/12 13:23:58 by ppotier          ###   ########.fr       */
+/*   Updated: 2023/01/12 13:46:17 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlxOS/mlx.h"
+#include "libft/libft.h"
 #include <unistd.h>
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-		ft_printf()
+	if (ft_strchr(argv[1],))
+	return (0);
 }
