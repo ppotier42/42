825f8157dfa7c5a1b260f5d12955e19a9ff05740
Pedroamiguel <pierre.podnieks@gmail.com>
2023-04-02 23:37:24 +0200

    ecole

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 13:09:12 by ppotier           #+#    #+#             */
-/*   Updated: 2023/03/30 14:19:11 by ppotier          ###   ########.fr       */
+/*   Updated: 2023/03/30 22:14:24 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ int	main(int ac, char **av)
 	int	i;
 
 	i = 0;
+	pid = ft_atoi_mini(av[1]);
 	if (ac == 3)
 	{
-		pid = ft_atoi_mini(av[1]);
 		while (av[2][i] != '\0')
 		{
 			ft_send(pid, av[2][i]);
