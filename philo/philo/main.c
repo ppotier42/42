3ac98d3cd78bb1cf1dcfc592b05788730247b646
Pierre 42 <pierre.podnieks@gmail.com>
2023-05-02 15:10:55 +0200

    je start philo

--- philo/philo/main.c
+++ philo/philo/main.c
@@ -6,19 +6,35 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:07:35 by ppotier           #+#    #+#             */
-/*   Updated: 2023/05/02 13:21:59 by ppotier          ###   ########.fr       */
+/*   Updated: 2023/05/02 14:05:56 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	ft_get_time(char **av, t_data *data)
+{
+	struct timeval te;
+	(void)av;
+	(void)data;
+    gettimeofday(&te, NULL); // get current time
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
+    printf("milliseconds: %lld\n", milliseconds);
+}
+
+// struct philo, struct table struct data ? 
 void	ft_init_philo(int ac, char **av)
 {
-	int	nb_philo;
-	int	time_to_die;
+	(void)ac;
+	t_data	*data;
+	t_philo	*philo;
+	t_table	*table;
 
-	nb_philo = ft_atoi(av[1]);
-	
+	ft_get_time(av, data);
+	philo->philo = ft_atoi(av[1]);
+	// data->timetodie = gettimeofday(av[2]);
+	// data->timetoeat = ft_atoi(av[3]);
+	// data->timetosleep = ft_atoi(av[4]);
 }
 
 int	main(int ac, char **av)
--- philo/philo/philo.h
+++ philo/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: ppotier <ppotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:07:54 by ppotier           #+#    #+#             */
-/*   Updated: 2023/05/02 13:12:32 by ppotier          ###   ########.fr       */
+/*   Updated: 2023/05/02 14:06:19 by ppotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,30 @@
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+typedef struct s_data
+{
+	int	nb_philo;
+	int	timetodie;
+	int	timetoeat;
+	int	timetosleep;	
+}	t_data;
+
+typedef struct s_philo
+{
+	int				philo;
+	//mutex;
+	t_data			data;
+	struct s_philo	**next;
+}	t_philo;
+
+typedef struct s_table
+{
+	int		philo_eat;
+	// mutex; 2? imprime et manger
+	t_data	*data;
+}	t_table;
+
 
 // main.c
 int		main(int ac, char **av);
